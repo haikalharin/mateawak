c0727c6f9353b4cbf5f5180995658ef40d587388
MuhammadHaikalHarin <v-m.haikal.harin@bukittechnology.com>
2024-02-27 23:08:58 +0700

    update mission page

---
+++ lib/src/configs/navigations/navigation.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'navigation.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$goRouterHash() => r'510048257da472dd009a4c8e5c532762cb575806';
+
+/// See also [goRouter].
+@ProviderFor(goRouter)
+final goRouterProvider = Provider<GoRouter>.internal(
+  goRouter,
+  name: r'goRouterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$goRouterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GoRouterRef = ProviderRef<GoRouter>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
