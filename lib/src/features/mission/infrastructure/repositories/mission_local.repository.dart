ffc7cdf0158fd7eeccff125c7507546939c0a229
MuhammadHaikalHarin <v-m.haikal.harin@bukittechnology.com>
2024-03-23 04:02:19 +0700

    [feat] PPD-121 Update Mission List,  get image for task and view image

--- lib/src/features/mission/infrastructure/repositories/mission_local.repository.dart
+++ lib/src/features/mission/infrastructure/repositories/mission_local.repository.dart
@@ -23,20 +23,20 @@ FutureOr<List<GamificationResponseRemote>> getMissionRemote(GetMissionLocalRef r
   List<GamificationResponseRemote> listResponse = [];
   List<GamificationResponseRemote> listResponseFinal = [];
   List<GamificationResponseRemote> listAfterInputImage = [];
-  const rawMissionDummy = Constant.rawMissionDummy;
-  // final userModel = await ref.read(helperUserProvider).getUserProfile();
-  // final latestSyncDate = ref.read(latestSyncDateState.notifier).state;
-  // final response = await connect.post(
-  //   modul: ModuleType.etamkawaGamification,
-  //   path: "api/mission/get_employee_mission?${Constant.apiVer}",
-  //   body: {
-  //     "employeeId": userModel?.employeeID,
-  //     "requestDate": latestSyncDate
-  //     //"requestDate": '2024-03-01T03:55:58.918Z'
-  //   }
-  // );
-  // for (var element in response.result?.content) {
-  for (var element in rawMissionDummy) {
+  // const rawMissionDummy = Constant.rawMissionDummy;
+  final userModel = await ref.read(helperUserProvider).getUserProfile();
+  final latestSyncDate = ref.read(latestSyncDateState.notifier).state;
+  final response = await connect.post(
+    modul: ModuleType.etamkawaGamification,
+    path: "api/mission/get_employee_mission?${Constant.apiVer}",
+    body: {
+      "employeeId": userModel?.employeeID,
+      "requestDate": latestSyncDate
+      //"requestDate": '2024-03-01T03:55:58.918Z'
+    }
+  );
+  for (var element in response.result?.content) {
+  // for (var element in rawMissionDummy) {
     final result = GamificationResponseRemote.fromJson(element);
     listResponse.add(result);
   }
