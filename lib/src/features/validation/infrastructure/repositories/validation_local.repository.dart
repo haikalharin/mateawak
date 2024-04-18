35e0790efe05692c6472c71fb900faa6d7ce7aeb
Abdu ar Rahman <abdu.arrahman@bukittechnology.com>
2024-04-19 05:05:26 +0700

    [feat] PPD 59 sync from api validation list

--- lib/src/features/validation/infrastructure/repositories/validation_local.repository.dart
+++ lib/src/features/validation/infrastructure/repositories/validation_local.repository.dart
@@ -1,6 +1,9 @@
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
+import 'package:module_etamkawa/src/constants/function_utils.dart';
 import 'package:module_etamkawa/src/features/validation/domain/validation_response.remote.dart';
 import 'package:module_shared/module_shared.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -22,129 +25,129 @@ FutureOr<List<ValidationResponseRemote>> getValidationRemote(GetValidationLocalR
     List<ValidationResponseRemote> listResponse = [];
     List<ValidationResponseRemote> listResponseFinal = [];
     List<ValidationResponseRemote> listAfterInputImage = [];
-    const rawValidationDummy = Constant.rawValidationDummy;
+    //const rawValidationDummy = Constant.rawValidationDummy;
     final userModel = await ref.read(helperUserProvider).getUserProfile();
-    // final response = await connect.post(
-    //     modul: ModuleType.etamkawaGamification,
-    //     path: "api/mission/get_employee_mission_by_param?${Constant.apiVer}",
-    //     body: {
-    //       "validatorId": userModel?.employeeID,
-    //     }
-    // );
-    //for (var element in response.result?.content) {
-       for (var element in rawValidationDummy) {
+    final response = await connect.post(
+        modul: ModuleType.etamkawaGamification,
+        path: "api/mission/get_employee_mission_by_param?${Constant.apiVer}",
+        body: {
+          "validatorId": userModel?.employeeID,
+        }
+    );
+    for (var element in response.result?.content) {
+       //for (var element in rawValidationDummy) {
       final result = ValidationResponseRemote.fromJson(element);
       listResponse.add(result);
     }
     
     var repo = ref.watch(getValidationLocalProvider.future);
     
-    // for (var element in listResponse) {
-    //   await AsyncValue.guard(() => repo).then((value) async {
-    //     if ((value.value ?? []).isNotEmpty) {
-    //       bool exists = (value.value ?? []).any((item) =>
-    //       item.employeeMissionId == element.employeeMissionId);
-    //       if (!exists) {
-    //         listResponseFinal.add(element);
-    //       }
-    //     } else {
-    //       listResponseFinal.add(element);
-    //     }
-    //   });
-    // }
+    for (var element in listResponse) {
+      await AsyncValue.guard(() => repo).then((value) async {
+        if ((value.value ?? []).isNotEmpty) {
+          bool exists = (value.value ?? []).any((item) =>
+          item.employeeMissionId == element.employeeMissionId);
+          if (!exists) {
+            listResponseFinal.add(element);
+          }
+        } else {
+          listResponseFinal.add(element);
+        }
+      });
+    }
     
-    // List<ValidationResponseRemote> listResponseFinalFix = listResponseFinal
-    //     .toSet().toList();
-    // int index = 0;
-    // for (var element in listResponseFinalFix) {
-    //   List<TaskDatum> listTask =
-    //       element.chapterData?.single.missionData?.single.taskData ?? [];
-    //   int indexTask = 0;
-    //   List<TaskDatum> taskData = [];
-    //   for (var element in listTask) {
-    //     File file = File('');
-    //     if (element.attachmentPath == null) {
-    //       if (element.attachmentUrl != null) {
-    //         final response = await connect.downloadImage(
-    //           url: element.attachmentUrl ?? '',
-    //         );
-    //         response.data;
-    //         file = await asyncMethodSaveFile(response.data);
-    //       }
-    //       taskData.add(TaskDatum(taskId: element.taskId,
-    //           missionId: element.missionId,
-    //           attachmentId: element.attachmentId,
-    //           attachmentUrl: element.attachmentUrl,
-    //           attachmentPath: file.path,
-    //           taskCode: element.taskCode,
-    //           taskGroup: element.taskGroup,
-    //           taskCaption: element.taskCaption,
-    //           taskTypeCode: element.taskTypeCode,
-    //           taskTypeName: element.taskTypeName,
-    //           taskReward: element.taskReward));
-    //       indexTask++;
-    //     } else {
-    //       taskData.add(TaskDatum(taskId: element.taskId,
-    //           missionId: element.missionId,
-    //           attachmentId: element.attachmentId,
-    //           attachmentUrl: element.attachmentUrl,
-    //           attachmentPath: element.attachmentPath,
-    //           taskCode: element.taskCode,
-    //           taskGroup: element.taskGroup,
-    //           taskCaption: element.taskCaption,
-    //           taskTypeCode: element.taskTypeCode,
-    //           taskTypeName: element.taskTypeName,
-    //           taskReward: element.taskReward));
-    //     }
-    //   }
-    //   listAfterInputImage.add(ValidationResponseRemote(
-    //       employeeMissionId: element.employeeMissionId,
-    //       missionId: element.missionId,
-    //       missionStatusCode: element.missionStatusCode,
-    //       missionStatus: element.missionStatus,
-    //       startedDate: element.startedDate,
-    //       dueDate: element.dueDate,
-    //       submittedBy: element.submittedBy,
-    //       submittedDate: element.submittedDate,
-    //       completedBy: element.completedBy,
-    //       completedDate: element.completedDate,
-    //       chapterData: [ChapterDatum(
-    //         chapterId: element.chapterData?.single.chapterId,
-    //         chapterCode: element.chapterData?.single.chapterCode,
-    //         chapterName: element.chapterData?.single.chapterName,
-    //         chapterGoal: element.chapterData?.single.chapterGoal,
-    //         competencyCode: element.chapterData?.single.competencyCode,
-    //         competencyName: element.chapterData?.single.competencyName,
-    //         peopleCategoryCode: element.chapterData?.single.peopleCategoryCode,
-    //         peopleCategoryName: element.chapterData?.single.peopleCategoryName,
-    //         missionData: [
-    //           MissionDatum(
-    //             missionId: element.chapterData?.single.missionData?.single
-    //                 .missionId,
-    //             chapterId: element.chapterData?.single.missionData?.single
-    //                 .chapterId,
-    //             missionCode: element.chapterData?.single.missionData?.single
-    //                 .missionCode,
-    //             missionName: element.chapterData?.single.missionData?.single
-    //                 .missionName,
-    //             missionInstruction: element.chapterData?.single.missionData
-    //                 ?.single.missionInstruction,
-    //             missionDuration: element.chapterData?.single.missionData?.single
-    //                 .missionDuration,
-    //             missionActiveOnDay: element.chapterData?.single.missionData
-    //                 ?.single.missionActiveOnDay,
-    //             missionTypeCode: element.chapterData?.single.missionData?.single
-    //                 .missionTypeCode,
-    //             missionTypeName: element.chapterData?.single.missionData?.single
-    //                 .missionTypeName,
-    //             missionReward: element.chapterData?.single.missionData?.single
-    //                 .missionReward,
-    //             taskData: taskData,)
-    //         ],
-    //       )
-    //       ]));
-    //   index++;
-    // }
+    List<ValidationResponseRemote> listResponseFinalFix = listResponseFinal
+        .toSet().toList();
+    int index = 0;
+    for (var element in listResponseFinalFix) {
+      List<TaskValidationDatum> listTask =
+          element.chapterData?.single.missionData?.single.taskData ?? [];
+      int indexTask = 0;
+      List<TaskValidationDatum> taskData = [];
+      for (var element in listTask) {
+        File file = File('');
+        if (element.attachmentPath == null) {
+          if (element.attachmentUrl != null) {
+            final response = await connect.downloadImage(
+              url: element.attachmentUrl ?? '',
+            );
+            response.data;
+            file = await asyncMethodSaveFile(response.data);
+          }
+          taskData.add(TaskValidationDatum(taskId: element.taskId,
+              missionId: element.missionId,
+              attachmentId: element.attachmentId,
+              attachmentUrl: element.attachmentUrl,
+              attachmentPath: file.path,
+              taskCode: element.taskCode,
+              taskGroup: element.taskGroup,
+              taskCaption: element.taskCaption,
+              taskTypeCode: element.taskTypeCode,
+              taskTypeName: element.taskTypeName,
+              taskReward: element.taskReward));
+          indexTask++;
+        } else {
+          taskData.add(TaskValidationDatum(taskId: element.taskId,
+              missionId: element.missionId,
+              attachmentId: element.attachmentId,
+              attachmentUrl: element.attachmentUrl,
+              attachmentPath: element.attachmentPath,
+              taskCode: element.taskCode,
+              taskGroup: element.taskGroup,
+              taskCaption: element.taskCaption,
+              taskTypeCode: element.taskTypeCode,
+              taskTypeName: element.taskTypeName,
+              taskReward: element.taskReward));
+        }
+      }
+      listAfterInputImage.add(ValidationResponseRemote(
+          employeeMissionId: element.employeeMissionId,
+          missionId: element.missionId,
+          missionStatusCode: element.missionStatusCode,
+          missionStatus: element.missionStatus,
+          startedDate: element.startedDate,
+          dueDate: element.dueDate,
+          submittedBy: element.submittedBy,
+          submittedDate: element.submittedDate,
+          completedBy: element.completedBy,
+          completedDate: element.completedDate,
+          chapterData: [ChapterValidationDatum(
+            chapterId: element.chapterData?.single.chapterId,
+            chapterCode: element.chapterData?.single.chapterCode,
+            chapterName: element.chapterData?.single.chapterName,
+            chapterGoal: element.chapterData?.single.chapterGoal,
+            competencyCode: element.chapterData?.single.competencyCode,
+            competencyName: element.chapterData?.single.competencyName,
+            peopleCategoryCode: element.chapterData?.single.peopleCategoryCode,
+            peopleCategoryName: element.chapterData?.single.peopleCategoryName,
+            missionData: [
+              MissionValidationDatum(
+                missionId: element.chapterData?.single.missionData?.single
+                    .missionId,
+                chapterId: element.chapterData?.single.missionData?.single
+                    .chapterId,
+                missionCode: element.chapterData?.single.missionData?.single
+                    .missionCode,
+                missionName: element.chapterData?.single.missionData?.single
+                    .missionName,
+                missionInstruction: element.chapterData?.single.missionData
+                    ?.single.missionInstruction,
+                missionDuration: element.chapterData?.single.missionData?.single
+                    .missionDuration,
+                missionActiveOnDay: element.chapterData?.single.missionData
+                    ?.single.missionActiveOnDay,
+                missionTypeCode: element.chapterData?.single.missionData?.single
+                    .missionTypeCode,
+                missionTypeName: element.chapterData?.single.missionData?.single
+                    .missionTypeName,
+                missionReward: element.chapterData?.single.missionData?.single
+                    .missionReward,
+                taskData: taskData,)
+            ],
+          )
+          ]));
+      index++;
+    }
     
     await isarInstance.writeTxn(() async {
       await isarInstance.validationResponseRemotes.putAll(
