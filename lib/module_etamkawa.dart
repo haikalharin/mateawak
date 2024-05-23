22a1f62f0c3482a316e9d8fd2705dd070c1bba05
MuhammadHaikalHarin <v-m.haikal.harin@bukittechnology.com>
2024-05-24 05:02:49 +0700

    [feat] rollback

--- lib/module_etamkawa.dart
+++ lib/module_etamkawa.dart
@@ -1,6 +1,5 @@
 library module_etamkawa;
 
-export 'package:module_etamkawa/src/configs/translations/etamkawa.translate.dart';
 export 'package:module_etamkawa/src/configs/navigations/navigation.dart';
 export 'package:module_etamkawa/src/configs/navigations/routes.dart';
 export 'package:module_etamkawa/src/features/offline_mode/presentation/controller/offline_mode_etamkawa.controller.dart';
